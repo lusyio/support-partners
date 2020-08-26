92f638cff4c8d06b57309552f8d5038a1a3e81a4
skripkaru <skripkaru.evgeniy@gmail.com>
2020-08-26 11:17:52 +0300

    Revert "new-mainscreen"
    This reverts commit a5831f530eedec3d2e2b039e7f83416524b673a0.

--- wp-content/themes/storefront-child/header.php
+++ wp-content/themes/storefront-child/header.php
@@ -28,11 +28,20 @@
     <header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
 
         <div class="container">
-            <nav class="navbar navbar-light navbar-expand-xl p-0 justify-content-between">
+            <nav class="navbar navbar-dark navbar-expand-xl p-0 justify-content-between">
                 <div class="navbar-brand">
-						<a href="#">
-					  <img src="/wp-content/themes/storefront-child/svg/logo.svg" alt="logo">
-				  </a>
+					<?php if ( get_theme_mod( 'wp_bootstrap_starter_logo' ) ): ?>
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?= esc_url( get_bloginfo( 'name' ) ); ?>
+                            <img src="<?= esc_url( get_theme_mod( 'wp_bootstrap_starter_logo' ) ); ?>"
+                                 alt="<?= esc_attr( get_bloginfo( 'name' ) ); ?>">
+                        </a>
+					<?php else : ?>
+                        <a class="site-title text-decoration-none"
+                           href="<?php echo esc_url( home_url( '/' ) ); ?>">
+							<?= esc_url( bloginfo( 'name' ) ); ?>
+                            <p class="mb-0 site-description"><?php bloginfo( 'description' ); ?></p>
+                        </a>
+					<?php endif; ?>
                 </div>
 
                 <div class="d-flex">
@@ -49,8 +58,22 @@
 						'walker'          => new wp_bootstrap_navwalker()
 					) );
 					?>
+					<?php get_search_form() ?>
+					<?php if ( class_exists( 'WooCommerce' ) ):?>
+                        <div class="s-header__basket-wr woocommerce mr-1 mr-sm-4 mt-auto mb-auto z-5 position-relative">
+							<?php
+							global $woocommerce; ?>
+                            <a href="<?php echo $woocommerce->cart->get_cart_url() ?>"
+                               class="basket-btn basket-btn_fixed-xs text-decoration-none position-relative">
+                        <span class="basket-btn__label"><img src="/wp-content/themes/storefront-child/svg/cart.svg"
+                                                             alt=""></span>
+								<?php if ( sprintf( $woocommerce->cart->cart_contents_count ) != 0 ): ?>
+                                    <span class="basket-btn__counter"><?php echo sprintf( $woocommerce->cart->cart_contents_count ); ?></span>
+								<?php endif; ?>
+                            </a>
+                        </div>
+					<?php endif; ?>
 
-						<?= get_field('phone', 21) ?>
                     <div class="outer-menu">
                         <button class="navbar-toggler position-relative" type="button" style="z-index: 1">
                             <span class="navbar-toggler-icon"></span>
