06ea67d50d455c54692e2feaca64a3e2f887f9b7
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2020-09-01 18:13:18 +0300

    Поправил svg и сделал ссылки

--- wp-content/themes/storefront-child/page-templates/events.php
+++ wp-content/themes/storefront-child/page-templates/events.php
@@ -24,17 +24,17 @@ Template Post Type: post, page, product
    <div class="container">
       <h2 class="upcoming-events__title">Предстоящие мероприятия</h2>
       <div class="upcoming-events__inner">
-         <div class="upcoming-events__img">
+         <a href="#" class="upcoming-events__img">
             <img src="/wp-content/themes/storefront-child/img/events/img-1.png" alt="dreamteamforum">
-         </div>
+         </a>
          <div class="upcoming-events__content">
             <div class="upcoming-events__content-title">#dreamteamforum</div>
             <div class="upcoming-events__content-descr">Масштабная конференция о создании <br> сильных команд</div>
             <div class="upcoming-events__content-date">08.11.2020</div>
             <a href="#" class="upcoming-events__content-link">
                Узнать больше
-               <svg class="arrow">
-                  <use xlink:href="#arrow-white"></use>
+               <svg class="icon">
+                  <use xlink:href="#arrow"></use>
                </svg>
             </a>
          </div>
@@ -47,7 +47,7 @@ Template Post Type: post, page, product
       <h2 class="past-events__title">Прошедшие мероприятия</h2>
       <div class="row">
          <div class="col-md-4">
-            <div class="past-events__item">
+            <a href="#" class="past-events__item">
                <div class="past-events__item-header">
                   <div class="past-events__item-img">
                      <img src="/wp-content/themes/storefront-child/img/events/img-2.png" alt="">
@@ -60,10 +60,10 @@ Template Post Type: post, page, product
                   </div>
                   <div class="past-events__item-descr">Бизнес-встреча об управлении командой компании в экстремальных условиях</div>
                </div>
-            </div>
+            </a>
          </div>
          <div class="col-md-4">
-            <div class="past-events__item">
+            <a href="#" class="past-events__item">
                <div class="past-events__item-header">
                   <div class="past-events__item-img">
                      <img src="/wp-content/themes/storefront-child/img/events/img-2.png" alt="">
@@ -76,10 +76,10 @@ Template Post Type: post, page, product
                   </div>
                   <div class="past-events__item-descr">Открытая программа для руководителей, которые <br> хотят внедрить в бизнес инновационные практики управления</div>
                </div>
-            </div>
+            </a>
          </div>
          <div class="col-md-4">
-            <div class="past-events__item">
+            <a href="#" class="past-events__item">
                <div class="past-events__item-header">
                   <div class="past-events__item-img">
                      <img src="/wp-content/themes/storefront-child/img/events/img-2.png" alt="">
@@ -92,19 +92,11 @@ Template Post Type: post, page, product
                   </div>
                   <div class="past-events__item-descr">Самое яркое и полезное <br> HR-событие весны для тех, <br> кто развивается и делает карьеру в HR</div>
                </div>
-            </div>
+            </a>
          </div>
       </div>
    </div>
 </section>
-
-
-
-<svg width="0" height="0" class="hidden">
-   <symbol viewBox="0 0 82 10" fill="none" xmlns="http://www.w3.org/2000/svg" id="arrow-white">
-      <path d="M81.46 5.46a.65.65 0 0 0 0-.92L77.323.404a.65.65 0 1 0-.92.919L80.082 5l-3.677 3.677a.65.65 0 0 0 .919.92L81.46 5.46zM0 5.65h81v-1.3H0v1.3z" fill="#fff"></path>
-   </symbol>
-</svg>
 
 
 <?php get_footer(); ?>
