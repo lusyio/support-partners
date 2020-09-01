da870c0bc5eeabc83fa6ffc67e4fc99ea9046517
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2020-09-01 13:20:27 +0300

    Сделал страницу Кейсы

---
+++ wp-content/themes/storefront-child/page-templates/cases.php
@@ -0,0 +1,183 @@
+<?php
+/*
+Template Name: cases
+Template Post Type: post, page, product
+*/
+?>
+
+<?php get_header(); ?>
+
+
+<div class="container">
+   <ul class="breadcrumb-primary">
+      <li>
+         <a class="breadcrumb-primary__link" href="#">Главная</a>
+      </li>
+      <li class="breadcrumb-primary__separator">/</li>
+      <li>
+         <a class="breadcrumb-primary__link breadcrumb-primary__link_active" href="#">Кейсы</a>
+      </li>
+   </ul>
+</div>
+
+
+<section class="work">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-4">
+            <a href="#" class="work__item">
+               <div class="work__item-title">название проекта 1</div>
+               <div class="work__item-img">
+                  <img src="/wp-content/themes/storefront-child/svg/clients/skolkovo.svg" alt="skolkovo">
+               </div>
+               <div class="work__item-wrap">
+                  <div class="work__item-descr">
+                     Путем быстрого и качественного поиска руководителей мы позволяем нашим клиентам решать их бизнес-задачи
+                     и добиваться поставленных целей.
+                  </div>
+                  <a href="#" class="work__item-link">
+                     Узнать больше
+                     <svg class="arrow">
+                        <use xlink:href="#arrow"></use>
+                     </svg>
+                  </a>
+               </div>
+            </a>
+         </div>
+         <div class="col-md-4">
+            <a href="#" class="work__item">
+               <div class="work__item-title">название проекта 2</div>
+               <div class="work__item-img">
+                  <img src="/wp-content/themes/storefront-child/svg/clients/knauf.svg" alt="knauf">
+               </div>
+               <div class="work__item-wrap">
+                  <div class="work__item-descr">
+                     Путем быстрого и качественного поиска руководителей мы позволяем нашим клиентам решать их бизнес-задачи
+                     и добиваться поставленных целей.
+                  </div>
+                  <a href="#" class="work__item-link">
+                     Узнать больше
+                     <svg class="arrow">
+                        <use xlink:href="#arrow"></use>
+                     </svg>
+                  </a>
+               </div>
+            </a>
+         </div>
+         <div class="col-md-4">
+            <a href="#" class="work__item">
+               <div class="work__item-title">название проекта 3</div>
+               <div class="work__item-img">
+                  <img src="/wp-content/themes/storefront-child/svg/clients/avon.svg" alt="avon">
+               </div>
+               <div class="work__item-wrap">
+                  <div class="work__item-descr">
+                     Путем быстрого и качественного поиска руководителей мы позволяем нашим клиентам решать их бизнес-задачи
+                     и добиваться поставленных целей.
+                  </div>
+                  <a href="#" class="work__item-link">
+                     Узнать больше
+                     <svg class="arrow">
+                        <use xlink:href="#arrow"></use>
+                     </svg>
+                  </a>
+               </div>
+            </a>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col-md-4">
+            <a href="#" class="work__item">
+               <div class="work__item-title">название проекта 4</div>
+               <div class="work__item-img">
+                  <img src="/wp-content/themes/storefront-child/svg/clients/luhta.svg" alt="luhta">
+               </div>
+               <div class="work__item-wrap">
+                  <div class="work__item-descr">
+                     Путем быстрого и качественного поиска руководителей мы позволяем нашим клиентам решать их бизнес-задачи
+                     и добиваться поставленных целей.
+                  </div>
+                  <a href="#" class="work__item-link">
+                     Узнать больше
+                     <svg class="arrow">
+                        <use xlink:href="#arrow"></use>
+                     </svg>
+                  </a>
+               </div>
+            </a>
+         </div>
+         <div class="col-md-4">
+            <a href="#" class="work__item">
+               <div class="work__item-title">название проекта 5</div>
+               <div class="work__item-img">
+                  <img src="/wp-content/themes/storefront-child/svg/clients/unicredit.svg" alt="unicredit">
+               </div>
+               <div class="work__item-wrap">
+                  <div class="work__item-descr">
+                     Путем быстрого и качественного поиска руководителей мы позволяем нашим клиентам решать их бизнес-задачи
+                     и добиваться поставленных целей.
+                  </div>
+                  <a href="#" class="work__item-link">
+                     Узнать больше
+                     <svg class="arrow">
+                        <use xlink:href="#arrow"></use>
+                     </svg>
+                  </a>
+               </div>
+            </a>
+         </div>
+         <div class="col-md-4">
+            <a href="#" class="work__item">
+               <div class="work__item-title">название проекта 6</div>
+               <div class="work__item-img">
+                  <img src="/wp-content/themes/storefront-child/svg/clients/cargill.svg" alt="cargill">
+               </div>
+               <div class="work__item-wrap">
+                  <div class="work__item-descr">
+                     Путем быстрого и качественного поиска руководителей мы позволяем нашим клиентам решать их бизнес-задачи
+                     и добиваться поставленных целей.
+                  </div>
+                  <a href="#" class="work__item-link">
+                     Узнать больше
+                     <svg class="arrow">
+                        <use xlink:href="#arrow"></use>
+                     </svg>
+                  </a>
+               </div>
+            </a>
+         </div>
+      </div>
+   </div>
+</section>
+
+<div class="container">
+   <ul class="pagination-primary">
+      <li>
+         <a class="pagination-primary__link" href="#">1</a>
+      </li>
+      <li>
+         <a class="pagination-primary__link" href="#">2</a>
+      </li>
+      <li>
+         <a class="pagination-primary__link" href="#">3</a>
+      </li>
+      <li>
+         <a class="pagination-primary__link" href="#">...</a>
+      </li>
+   </ul>
+</div>
+
+
+
+<!-- Svg Sprite -->
+
+<svg width="0" height="0" class="hidden">
+   <symbol viewBox="0 0 82 10" fill="none" xmlns="http://www.w3.org/2000/svg" id="arrow">
+      <path d="M81.46 5.46a.65.65 0 0 0 0-.92L77.323.404a.65.65 0 1 0-.92.919L80.082 5l-3.677 3.677a.65.65 0 0 0 .919.92L81.46 5.46zM0 5.65h81v-1.3H0v1.3z" fill="none"></path>
+   </symbol>
+</svg>
+
+
+
+
+<?php get_footer(); ?>
