c31d2e433ac43aebc18d2281a56c2f93e9fd403f
skripkaru <55709310+skripkaru@users.noreply.github.com>
2020-09-02 10:50:18 +0300

    Сделал страницу Новости (#10)

---
+++ wp-content/themes/storefront-child/page-templates/news.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Template Name: news
+Template Post Type: post, page, product
+*/
+?>
+
+<?php get_header(); ?>
+
+
+<div class="container">
+   <ul class="breadcrumb-primary">
+      <li>
+         <a class="breadcrumb-primary__link" href="#">Главная</a>
+      </li>
+      <li class="breadcrumb-primary__separator">/</li>
+      <li>
+         <a class="breadcrumb-primary__link breadcrumb-primary__link_active" href="#">Hовости</a>
+      </li>
+   </ul>
+</div>
+
+<div class="news">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-md-5">
+            <a href="#" class="news__img">
+               <img src="/wp-content/themes/storefront-child/img/events/img-2.png" alt="">
+            </a>
+         </div>
+         <div class="col-md-7">
+            <div class="news__content">
+               <div class="news__content-title">новость</div>
+               <div class="news__content-date">16.03.2020</div>
+               <div class="news__content-descr">
+                  Компания Support Partners успешно занимается поиском <br> руководителей высшего (executive search) и среднего звена для <br>
+                  российского бизнеса, иностранных компаний и государственных <br> корпораций
+               </div>
+               <a href="#" class="news__content-link">
+                  <svg class="icon">
+                     <use xlink:href="#arrow"></use>
+                  </svg>
+               </a>
+            </div>
+         </div>
+      </div>
+      <div class="row align-items-center">
+         <div class="col-md-5">
+            <a href="#" class="news__img">
+               <img src="/wp-content/themes/storefront-child/img/events/img-2.png" alt="">
+            </a>
+         </div>
+         <div class="col-md-7">
+            <div class="news__content">
+               <div class="news__content-title">новость 2</div>
+               <div class="news__content-date">16.03.2020</div>
+               <div class="news__content-descr">
+                  Компания Support Partners успешно занимается поиском <br> руководителей высшего (executive search) и среднего звена для <br>
+                  российского бизнеса, иностранных компаний и государственных <br> корпораций
+               </div>
+               <a href="#" class="news__content-link">
+                  <svg class="icon">
+                     <use xlink:href="#arrow"></use>
+                  </svg>
+               </a>
+            </div>
+         </div>
+      </div>
+      <div class="row align-items-center">
+         <div class="col-md-5">
+            <a href="#" class="news__img">
+               <img src="/wp-content/themes/storefront-child/img/events/img-2.png" alt="">
+            </a>
+         </div>
+         <div class="col-md-7">
+            <div class="news__content">
+               <div class="news__content-title">новость 3</div>
+               <div class="news__content-date">16.03.2020</div>
+               <div class="news__content-descr">
+                  Компания Support Partners успешно занимается поиском <br> руководителей высшего (executive search) и среднего звена для <br>
+                  российского бизнеса, иностранных компаний и государственных <br> корпораций
+               </div>
+               <a href="#" class="news__content-link">
+                  <svg class="icon">
+                     <use xlink:href="#arrow"></use>
+                  </svg>
+               </a>
+            </div>
+         </div>
+      </div>
+      <div class="row align-items-center">
+         <div class="col-md-5">
+            <a href="#" class="news__img">
+               <img src="/wp-content/themes/storefront-child/img/events/img-2.png" alt="">
+            </a>
+         </div>
+         <div class="col-md-7">
+            <div class="news__content">
+               <div class="news__content-title">новость 4</div>
+               <div class="news__content-date">16.03.2020</div>
+               <div class="news__content-descr">
+                  Компания Support Partners успешно занимается поиском <br> руководителей высшего (executive search) и среднего звена для <br>
+                  российского бизнеса, иностранных компаний и государственных <br> корпораций
+               </div>
+               <a href="#" class="news__content-link">
+                  <svg class="icon">
+                     <use xlink:href="#arrow"></use>
+                  </svg>
+               </a>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+
+<div class="container">
+   <ul class="pagination-primary">
+      <li>
+         <a class="pagination-primary__link" href="#">1</a>
+      </li>
+      <li>
+         <a class="pagination-primary__link" href="#">2</a>
+      </li>
+      <li>
+         <a class="pagination-primary__link" href="#">3</a>
+      </li>
+      <li>
+         <a class="pagination-primary__link" href="#">...</a>
+      </li>
+   </ul>
+</div>
+
+
+
+<?php get_footer(); ?>
