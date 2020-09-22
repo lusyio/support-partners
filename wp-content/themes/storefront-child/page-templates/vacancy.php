9ad78306b36043207e48b12b71e5bf489cfce8e8
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-09-22 15:39:21 +0300

    форма для вакансии

--- wp-content/themes/storefront-child/page-templates/vacancy.php
+++ wp-content/themes/storefront-child/page-templates/vacancy.php
@@ -46,35 +46,43 @@ Template Post Type: post, page, product
         <div class="vacancy__footer">
             <div class="container">
                 <h3 class="vacancy__footer-title">Откликнуться на вакансию</h3>
-                <div class="row no-gutters">
-                    <div class="col-md-8">
-                        <form action="#">
-                            <input type="text" name="name" placeholder="Имя Фамилия Отчество" required>
-                            <input type="tel" name="tel" placeholder="Мобильный телефон" required>
-                            <input type="email" name="email" placeholder="Электронная почта" required>
-                            <label for="check">
-                                <input type="checkbox" name="" id="check">
-                                Добавить сопроводительное письмо
-                            </label>
-                            <textarea name="message"
-                                      placeholder="Расскажите, почему вы хотите работать именно в этой компании"
-                                      required></textarea>
-                            <button class="btn-primary" type="submit">Отправить</button>
-                        </form>
-                    </div>
-                    <div class="col-md-4">
-                        <button class="vacancy__footer-file">
-                            <img src="/wp-content/themes/storefront-child/svg/pen.svg" alt="">
-                            <div>
-                                <span>Прикрепить файл с резюме</span>
-                                <span>до 5 Mb doc, docx, pdf</span>
-                            </div>
-                        </button>
-                        <input type="file" name="file">
-                    </div>
-                </div>
+                <?= do_shortcode('[caldera_form id="CF5f69eed40f993"]') ?>
+
+                <!--                <div class="row no-gutters">-->
+                <!--                    <div class="col-md-8">-->
+                <!--                        <form action="#">-->
+                <!--                            <input type="text" name="name" placeholder="Имя Фамилия Отчество" required>-->
+                <!--                            <input type="tel" name="tel" placeholder="Мобильный телефон" required>-->
+                <!--                            <input type="email" name="email" placeholder="Электронная почта" required>-->
+                <!--                            <label for="check">-->
+                <!--                                <input type="checkbox" name="" id="check">-->
+                <!--                                Добавить сопроводительное письмо-->
+                <!--                            </label>-->
+                <!--                            <textarea name="message"-->
+                <!--                                      placeholder="Расскажите, почему вы хотите работать именно в этой компании"-->
+                <!--                                      required></textarea>-->
+                <!--                            <button class="btn-primary" type="submit">Отправить</button>-->
+                <!--                        </form>-->
+                <!--                    </div>-->
+                <!--                    <div class="col-md-4">-->
+                <!--                        <button class="vacancy__footer-file">-->
+                <!--                            <img src="/wp-content/themes/storefront-child/svg/pen.svg" alt="">-->
+                <!--                            <div>-->
+                <!--                                <span>Прикрепить файл с резюме</span>-->
+                <!--                                <span>до 5 Mb doc, docx, pdf</span>-->
+                <!--                            </div>-->
+                <!--                        </button>-->
+                <!--                        <input type="file" name="file">-->
+                <!--                    </div>-->
+                <!--                </div>-->
             </div>
         </div>
     </div>
+
+    <script>
+        window.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('fld_1939953_1').value = <?= $post->post_title ?>;
+        })
+    </script>
 
 <?php get_footer(); ?>
