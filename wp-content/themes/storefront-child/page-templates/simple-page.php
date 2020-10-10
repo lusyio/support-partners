e4ff33f9a4c22519578b89f0dd9bbb51aacee172
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-10-10 12:12:28 +0300

    шаблон для простых страниц

---
+++ wp-content/themes/storefront-child/page-templates/simple-page.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: simple-page
+Template Post Type: post, page, product
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container pb-5">
+    <?php the_content() ?>
+</div>
+
+<?php get_footer(); ?>
